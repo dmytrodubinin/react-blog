99c15f0f226b27fadfaa047a5295158ef5d1ed76
Dmytro Dubinin <dimitry.dubinin@gmail.com>
2024-07-31 20:08:50 +0300

    add nodejs rest api with mongodb

---
+++ api/index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import multer from 'multer';
+
+import { router as authRoute } from './routes/auth.js';
+import { router as userRoute } from './routes/users.js';
+import { router as postRoute } from './routes/posts.js';
+import { router as categoryRoute } from './routes/categories.js';
+
+const app = express();
+
+// Get __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB connection successful'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+    // cb(null, 'logo.svg');
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
+
+app.listen(5000, () => {
+  console.log('Backend server started');
+});

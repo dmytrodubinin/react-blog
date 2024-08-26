1bf4c9c31fb12a27ba84ee6e05016a23c1d5acb3
Dmytro Dubinin <dimitry.dubinin@gmail.com>
2024-08-26 13:33:38 +0300

    fix: bullet formatting in preview

--- client/src/utils/htmlUtils.js
+++ client/src/utils/htmlUtils.js
@@ -4,7 +4,10 @@ export const addTailwindClassesToContent = (html) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, 'text/html');
 
-  const headers = doc.querySelectorAll('h1, h2, h3, h4, h5, h6, p');
+  const headers = doc.querySelectorAll(
+    'h1, h2, h3, h4, h5, h6, p, ul, ol, li, a',
+  );
+
   headers.forEach((el) => {
     switch (el.tagName.toLowerCase()) {
       case 'h1':
@@ -27,6 +30,22 @@ export const addTailwindClassesToContent = (html) => {
         break;
       case 'p':
         el.className = 'mt-2 mb-4 text-base leading-relaxed';
+        break;
+      case 'ul':
+        el.className = 'list-disc list-inside mt-2 mb-4';
+        break;
+      case 'ol':
+        el.className = 'list-decimal list-inside mt-2 mb-4';
+        break;
+      case 'li':
+        if (el.getAttribute('data-list') === 'bullet') {
+          el.className = 'list-disc list-inside ml-4';
+        } else {
+          el.className = 'ml-4';
+        }
+        break;
+      case 'a':
+        el.className = 'text-blue-600 hover:underline';
         break;
       default:
         break;
